99d4207109d13ceae13f29f5a08646d880cafaff
Denis Kulakov <kulakovdenis@mail.ru>
2023-10-12 23:11:23 +0300

    initial commit

---
+++ IOS_2308_Kulakov_HW_2/IOS_2308_Kulakov_HW_2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IOS_2308_Kulakov_HW_2
+//
+//  Created by Dev on 12.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS_2308_Kulakov_HW_2/IOS_2308_Kulakov_HW_2/IOS_2308_Kulakov_HW_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_2308_Kulakov_HW_2App.swift
+//  IOS_2308_Kulakov_HW_2
+//
+//  Created by Dev on 12.10.23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_2308_Kulakov_HW_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
